8788bf495d8e7c30aeb0768879ba41ac402c57e0
Betim <94181611+sniperking310@users.noreply.github.com>
2024-11-15 22:30:40 +0100

    Update Distance.java

--- src/main/java/logic/Distance.java
+++ src/main/java/logic/Distance.java
@@ -7,17 +7,17 @@ public class Distance {
     private int distance = 0 ;
 
     public int calculateDistance(Base base , Base banana){
-        int id , x , y , z , id_ , x_ , y_ , z_;
+        int id, x, y, z, id_, x_, y_, z_;
         id = base.getUid() ;
-   x = base.getPosition().getX();
-   y = base.getPosition().getY();
-   z = base.getPosition().getZ();
+        x = base.getPosition().getX();
+        y = base.getPosition().getY();
+        z = base.getPosition().getZ();
 
-   x_ = banana.getPosition().getX();
-   y_ = banana.getPosition().getY();
-   z_ = banana.getPosition().getZ();
+        x_ = banana.getPosition().getX();
+        y_ = banana.getPosition().getY();
+        z_ = banana.getPosition().getZ();
 
-   // Finish Here
+        // Finish Here
 
         return distance;
     }
