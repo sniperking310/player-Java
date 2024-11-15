1e1a39d649e24038d3dfd8e93be23844ff136294
Betim <94181611+sniperking310@users.noreply.github.com>
2024-11-15 20:56:24 +0100

    Update Strategy.java

--- src/main/java/logic/Strategy.java
+++ src/main/java/logic/Strategy.java
@@ -10,6 +10,7 @@ public class Strategy{
     
     public static List<PlayerAction> decide(GameState gameState){
         //TODO: add your logic here
+        //test
         List<PlayerAction> playerAction = new ArrayList<>();
         playerAction.add(new PlayerAction(0, 0, 0));
         return playerAction;
