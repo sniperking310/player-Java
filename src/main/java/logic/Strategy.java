6dfd151ec127f56286c08691f0c810298323e219
Betim <94181611+sniperking310@users.noreply.github.com>
2024-11-15 22:13:39 +0100

    Update Strategy.java

--- src/main/java/logic/Strategy.java
+++ src/main/java/logic/Strategy.java
@@ -11,7 +11,6 @@ public class Strategy{
     public static List<PlayerAction> decide(GameState gameState){
         
         List<PlayerAction> playerAction = new ArrayList<>();
-        System.out.println(gameState);
         playerAction.add(new PlayerAction(0, 0, 0));
         return playerAction;
     }
